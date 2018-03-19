16e5a0e646d8d693e0bcdc4047d4013dae1e9c2f
LucaGonella <lucagonella1996@gmail.com>
2018-03-19 15:39:17 +0100

    es_2

--- Lab1_es_2.py
+++ Lab1_es_2.py
@@ -1,4 +1,7 @@
 print("give me a string: ")
 string=input()
 L=len(string)
-print(string[0]+string[1]+string[L-2]+string[L-1])
+if L>3:
+    print(string[0]+string[1]+string[L-2]+string[L-1])
+else:
+    print("you have to enter a string longer than 3 characters")
