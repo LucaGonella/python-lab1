7aa9c484c38a2bdd8e03b2329a171ad8242cb4c2
LucaGonella <lucagonella1996@gmail.com>
2018-03-19 15:56:33 +0100

    es_3

---
+++ Lab1_es_3.py
@@ -0,0 +1,20 @@
+choice=0
+while choice!= 4:
+    print("1=insert a new task"
+          "2=remove a task"
+          "3=show all the tasks"
+          "4=close the program"
+          "whar do you want to do?")
+    choice=int(input())
+    if choice==1:
+        print("funzione 1")
+    elif choice==2:
+        print("funzione2")
+    elif choice == 3:
+        print("funzione2")
+    else :
+        print("your number is wrong")
+
+
+
+
